43011b2a3a9a7d7d5645a2a6086630ce09c59289
Andy Kohler <akohler@library.ucla.edu>
2017-05-03 14:52:33 -0700

    LX-184: Change SFTP info for deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
 # Set variables to make this easier to adapt to other projects.
-SFTP_SERVER=travisci-sftp.library.ucla.edu
-SFTP_USER=travisci
-SFTP_TARGET_DIR=build/laptop_availability
+SFTP_SERVER=appdeploy-sftp.library.ucla.edu
+SFTP_USER=appdeploy
+SFTP_TARGET_DIR=build/laptops
 SFTP_FILESPEC='*.war'
 
 # Decrypt our private key, using travis encryption.
 # -K and -iv apparently set by travis.
 openssl aes-256-cbc -K $encrypted_7270e623501c_key -iv $encrypted_7270e623501c_iv \
-  -in id_rsa_travisci.enc -out id_rsa_travisci -d
+  -in id_rsa_appdeploy.enc -out id_rsa_appdeploy -d
 
 # Set required permissions for decrypted private key
 # and move to required location.
-chmod 600 id_rsa_travisci
-mv id_rsa_travisci ~/.ssh/id_rsa
+chmod 600 id_rsa_appdeploy
+mv id_rsa_appdeploy ~/.ssh/id_rsa
 
 # Add sftp site to known_hosts to avoid permanent hang on first sftp connection
-ssh-keyscan travisci-sftp.library.ucla.edu >> ~/.ssh/known_hosts
+ssh-keyscan appdeploy-sftp.library.ucla.edu >> ~/.ssh/known_hosts
 
 # Upload file(s) to sftp site in project-specific directory
 
