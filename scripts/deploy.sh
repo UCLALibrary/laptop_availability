aaf675dd6bd7754e9fa32c7363d33642735a53ec
Andy Kohler <akohler@library.ucla.edu>
2017-05-04 13:12:32 -0700

    LX-184: Improve sftp deployment via sftp batch mode

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,9 @@
 # Set variables to make this easier to adapt to other projects.
 SFTP_SERVER=appdeploy-sftp.library.ucla.edu
 SFTP_USER=appdeploy
-SFTP_TARGET_DIR=build/laptops
-SFTP_FILESPEC='*.war'
+SFTP_PROJECT=laptops
+SFTP_TARGET_DIR=${SFTP_PROJECT}
+SFTP_FILESPEC=${SFTP_PROJECT}.war
 
 # Decrypt our private key, using travis encryption.
 # -K and -iv apparently set by travis.
@@ -20,11 +21,23 @@ mv id_rsa_appdeploy ~/.ssh/id_rsa
 ssh-keyscan appdeploy-sftp.library.ucla.edu >> ~/.ssh/known_hosts
 
 # Upload file(s) to sftp site in project-specific directory
+# Use sftp's batch mode (-b, with - for stdin) to throw/ignore errors:
+## commands starting with - will show error messages, but not cause sftp
+## to exit if an error occurs on those commands.  See man sftp for details. 
 
 (
+  # sftp will exit if cd fails - must have/use build directory
+  echo "cd build"
+  # Try to create project sub-directory, but don't throw error if it fails (presumably because it already exists)
+  echo "-mkdir ${SFTP_TARGET_DIR}"
+  # sftp will exit if cd fails
   echo "cd ${SFTP_TARGET_DIR}"
-  echo "rm ${SFTP_FILESPEC}"
+  # sftp will ignore errors if old file can't be removed (presumably doesn't exist)
+  echo "-rm ${SFTP_FILESPEC}"
+  # sftp will exit if put fails
   echo "put target/${SFTP_FILESPEC}"
+  # Show a little extra info for logging
+  echo "pwd"
   echo "ls -l"
-) | sftp ${SFTP_USER}@${SFTP_SERVER}
+) | sftp -b - ${SFTP_USER}@${SFTP_SERVER}
 
